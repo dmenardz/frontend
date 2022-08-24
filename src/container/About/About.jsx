a5fd0fbf7f1d0e4bd27280f32abc3fb00706f45f
Menardz <menardsdanny@gmail.com>
2022-08-24 19:55:22 +0800

    first commit

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, {useEffect ,useState} from 'react';
 import './About.scss';
-import 
+import {motion} from 'framer-motion';
+import { useEffect, useState } from 'react';
+
 
 const About = () => {
   return (
